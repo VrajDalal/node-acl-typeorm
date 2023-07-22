965e37cfbeb13ef846826da9a655a01343ce367f
EvilRATT <evilrattechnologies@gmail.com>
2023-07-22 19:29:50 +0530

    Header Crash Issue Fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,88 +1,129 @@
-import { apiEndPoint } from "../core/route"
-import { registerUserData, loginUserData, forgetPasswordData } from "../handler/auth"
-import { getUsersList, getUserById, createUserData, updateUserData, deleteUserData } from "../handler/user"
-import { Request, Response, Router } from "express"
-import { Auth } from "../utils/auth"
-import { ForgetPasswordSchema, LoginSchema, RegisterSchema } from "../validation-schemas/auth.schema"
-import { CreaterUserSchema } from "../validation-schemas/auth.schema"
-import { createRoleData, deleteRoleById, getRoleData, getRoleIdData, updateRoleById } from "../handler/role"
-import { createPermissionData, deletePermissionData, getPermissionByIdData, getPermissionsData, updatePermissionData } from "../handler/permission"
-import { ROLE } from "../utils/enum"
+import { apiEndPoint } from '../core/route';
+import {
+  registerUserData,
+  loginUserData,
+  forgetPasswordData,
+} from '../handler/auth';
+import {
+  getUsersList,
+  getUserById,
+  createUserData,
+  updateUserData,
+  deleteUserData,
+} from '../handler/user';
+import { Router } from 'express';
+import { Auth } from '../utils/auth';
+import {
+  ForgetPasswordSchema,
+  LoginSchema,
+  RegisterSchema,
+} from '../validation-schemas/auth.schema';
+import { CreaterUserSchema } from '../validation-schemas/auth.schema';
+import {
+  createRoleData,
+  deleteRoleById,
+  getRoleData,
+  getRoleIdData,
+  updateRoleById,
+} from '../handler/role';
+import {
+  createPermissionData,
+  deletePermissionData,
+  getPermissionByIdData,
+  getPermissionsData,
+  updatePermissionData,
+} from '../handler/permission';
 
-const router: Router = Router()
+const router: Router = Router();
 
 //user  routes
-router.get(apiEndPoint.user, Auth.is(ROLE.VIEW_USERS), async (req, res, next) => {
-    await getUsersList(req, res, next)
-})
+router.get(apiEndPoint.user, async (req, res, next) => {
+  await getUsersList(req, res, next);
+});
 
 router.get(apiEndPoint.userById, async (req, res, next) => {
-    await getUserById(req, res, next)
-})
+  await getUserById(req, res, next);
+});
 
-router.post(apiEndPoint.register, Auth.validateSchema(RegisterSchema), async (req, res) => {
-    await registerUserData(req, res)
-})
+router.post(
+  apiEndPoint.register,
+  Auth.validateSchema(RegisterSchema),
+  async (req, res) => {
+    await registerUserData(req, res);
+  }
+);
 
-router.post(apiEndPoint.login, Auth.validateSchema(LoginSchema), async (req, res) => {
-    await loginUserData(req, res)
-})
+router.post(
+  apiEndPoint.login,
+  Auth.validateSchema(LoginSchema),
+  async (req, res) => {
+    await loginUserData(req, res);
+  }
+);
 
-router.post(apiEndPoint.forget, Auth.validateSchema(ForgetPasswordSchema), async (req, res) => {
-    await forgetPasswordData(req, res)
-})
+router.post(
+  apiEndPoint.forget,
+  Auth.validateSchema(ForgetPasswordSchema),
+  async (req, res) => {
+    await forgetPasswordData(req, res);
+  }
+);
 
-router.post(apiEndPoint.user, Auth.validateSchema(CreaterUserSchema), async (req, res, next) => {
-    await createUserData(req, res, next)
-})
+router.post(
+  apiEndPoint.user,
+  Auth.validateSchema(CreaterUserSchema),
+  async (req, res, next) => {
+    await createUserData(req, res, next);
+  }
+);
 
 router.put(apiEndPoint.userById, async (req, res, next) => {
-    await updateUserData(req, res, next)
-})
+  await updateUserData(req, res, next);
+});
 
 router.delete(apiEndPoint.userById, async (req, res, next) => {
-    await deleteUserData(req, res, next)
-})
+  await deleteUserData(req, res, next);
+});
 
 //role routes
 router.get(apiEndPoint.role, async (req, res, next) => {
-    await getRoleData(req, res, next)
-})
+  await getRoleData(req, res, next);
+});
 
 router.get(apiEndPoint.roleById, async (req, res, next) => {
-    await getRoleIdData(req, res, next)
-})
+  await getRoleIdData(req, res, next);
+});
 
 router.post(apiEndPoint.role, async (req, res, next) => {
-    await createRoleData(req, res, next)
-})
+  await createRoleData(req, res, next);
+});
 
 router.put(apiEndPoint.roleById, async (req, res, next) => {
-    await updateRoleById(req, res, next)
-})
+  await updateRoleById(req, res, next);
+});
 
 router.delete(apiEndPoint.roleById, async (req, res, next) => {
-    await deleteRoleById(req, res, next)
-})
+  await deleteRoleById(req, res, next);
+});
 
 //permission routes
 router.get(apiEndPoint.permissions, async (req, res, next) => {
-    await getPermissionsData(req, res, next)
-})
+  await getPermissionsData(req, res, next);
+});
 
 router.get(apiEndPoint.permissionById, async (req, res, next) => {
-    await getPermissionByIdData(req, res, next)
-})
+  await getPermissionByIdData(req, res, next);
+});
 
 router.post(apiEndPoint.permissions, async (req, res, next) => {
-    await createPermissionData(req, res, next)
-})
+  await createPermissionData(req, res, next);
+});
 
 router.put(apiEndPoint.permissionById, async (req, res, next) => {
-    await updatePermissionData(req, res, next)
-})
+  await updatePermissionData(req, res, next);
+});
 
 router.delete(apiEndPoint.permissionById, async (req, res, next) => {
-    await deletePermissionData(req, res, next)
-})
-export default router
+  await deletePermissionData(req, res, next);
+});
+export default router;
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,233 +1,116 @@
-import { NextFunction, Request, Response } from "express"
-import Validator, { ValidationError } from "fastest-validator"
-import fs from "fs"
-import jwt, { DecodeOptions } from 'jsonwebtoken'
-import { JsonWebTokenError, JwtPayload } from "jsonwebtoken"
-import { NetConnectOpts } from "net"
-import { authHelper } from "../helper/auth.helper"
-import { userHelper } from "../helper/user.helper"
-import { decode } from "punycode"
-import { json } from "stream/consumers"
-import { error } from "console"
-import { promises } from "dns"
-import { datasource } from "../core/datasource"
-import { User } from "../entity/user.entity"
-import { Roles } from "../entity/role.entity"
-import { Permissions } from "../entity/permission.entity"
-import { ROLE } from "./enum"
+import { NextFunction, Request, Response } from 'express';
+import Validator, { ValidationError } from 'fastest-validator';
+import fs from 'fs';
+import jwt, { DecodeOptions } from 'jsonwebtoken';
+import { JsonWebTokenError, JwtPayload } from 'jsonwebtoken';
+import { NetConnectOpts } from 'net';
+import { authHelper } from '../helper/auth.helper';
+import { userHelper } from '../helper/user.helper';
+import { decode } from 'punycode';
+import { json } from 'stream/consumers';
+import { error } from 'console';
+import { promises } from 'dns';
+import { datasource } from '../core/datasource';
+import { User } from '../entity/user.entity';
+import { Roles } from '../entity/role.entity';
+import { Permissions } from '../entity/permission.entity';
+import { ROLE } from './enum';
 
 export class Auth {
-	// public static validateDatabase = async () => {
-	// 	const exists = await fs.existsSync('./assets/user.json')
-	// 	if (!exists) {
-	// 		await fs.writeFileSync('./assets/user.json', '[]')
-	// 	}
-	// }
+  // public static validateDatabase = async () => {
+  // 	const exists = await fs.existsSync('./assets/user.json')
+  // 	if (!exists) {
+  // 		await fs.writeFileSync('./assets/user.json', '[]')
+  // 	}
+  // }
 
-	public static getRequestParams = (req: Request) => {
-		const { body, query, params } = req
-		return { ...body, ...query, ...params }
-	}
-	//middleware
-	public static validateSchema(reqSchema: any = {}):
-		(req: Request, res: Response, next: NextFunction) => void {
-		const v: Validator = new Validator()
-		return (req: Request, res: Response, next: NextFunction) => {
-			const validate = v.compile(reqSchema)
-			const validateResponce = validate(req.body)
-			if (validateResponce == true) {
-				next()
-			} else {
-				const error = validateResponce as ValidationError[]
-				res.status(400).send({
-					code: 400,
-					message: 'Invalide Request Paramas',
-					type: 'E_BAD_REQUEST_PARAMS',
-					error: error
-				})
-			}
-		}
-	}
-	//guard
+  public static getRequestParams = (req: Request) => {
+    const { body, query, params } = req;
+    return { ...body, ...query, ...params };
+  };
+  //middleware
+  public static validateSchema(
+    reqSchema: any = {}
+  ): (req: Request, res: Response, next: NextFunction) => void {
+    const v: Validator = new Validator();
+    return (req: Request, res: Response, next: NextFunction) => {
+      const validate = v.compile(reqSchema);
+      const validateResponce = validate(req.body);
+      if (validateResponce == true) {
+        next();
+      } else {
+        const error = validateResponce as ValidationError[];
+        res.status(400).send({
+          code: 400,
+          message: 'Invalide Request Paramas',
+          type: 'E_BAD_REQUEST_PARAMS',
+          error: error,
+        });
+      }
+    };
+  }
+  //guard
 
-	public static async validateRole2(
-		requiredPermission: any = {}
-	): Promise<(req: Request, res: Response, next: NextFunction) => void> {
-		const v: Validator = new Validator();
-		return async (req: Request, res: Response, next: NextFunction) => {
-			const headerAuth = req.headers.authorization;
-			if (!headerAuth) {
-				return res.status(400).json({
-					message: 'invalid token',
-				});
-			}
-			const replaceHeaders = headerAuth.replace('Bearer ', '');
-			//Decode JWT => from USER DATA
-			const decoded: any = jwt.decode(replaceHeaders);
-			const roleId = decoded.found.role.id;
-			try {
-				const roleFind = await datasource.getRepository(Roles).findOne({
-					relations: {
-						permission: true
-					},
-					where: {
-						id: roleId
-					}
-				})
-				console.log(roleFind)
-				// const found = roleFind.find((role:any) => decodedRole === roleFinder)
-				if (roleFind) {
-
-					const findPermission = roleFind.permission.some((permission: any) => permission.name === requiredPermission);
-					console.log(findPermission)
-					if (findPermission) {
-						next
-					} else {
-						res.status(403).send({
-							code: 403,
-							message: "You don't have enough permission",
-						})
-					}
-				} else {
-					res.status(400).send({
-						code: 400,
-						message: "Invalid Role",
-					})
-				}
-			} catch (error: any) {
-				res.status(500).send({
-					code: 500,
-					message: error.message,
-					stack: error.stack,
-					error: error
-				})
-			}
-		};
-	}
-
-
-	public static validateRole = async (req: Request, res: Response, next: NextFunction, requiredPermission: string) => {
-		//Get JWT From Headers
-		//check null or not if not than replace bearer with blank string
-		const headerAuth = req.headers.authorization
-		// console.log(headerAuth);
-		if (!headerAuth) {
-			return res.status(400).json({
-				message: 'invalid token'
-			})
-		}
-		const replaceHeaders = headerAuth.replace('Bearer ', '')
-		// console.log(replaceHeaders)
-		//Decode JWT => from USER DATA
-		const decoded: any = jwt.decode(replaceHeaders)
-		// console.log(decoded)
-		const roleId = decoded.found.roles.id
-		// console.log(roleId);
-		//Find Role from role table -> Invalid Role
-		try {
-			const roleFind = await datasource.getRepository(Roles).findOne({
-				relations: {
-					permission: true
-				},
-				where: {
-					id: roleId
-				}
-			})
-			console.log(roleFind)
-			// const found = roleFind.find((role:any) => decodedRole === roleFinder)
-			if (roleFind) {
-
-				const findPermission = roleFind.permission.some((permission: any) => permission.name === requiredPermission);
-				console.log(findPermission)
-				if (findPermission) {
-					next
-				} else {
-					res.status(403).send({
-						code: 403,
-						message: "You don't have enough permission",
-					})
-				}
-			} else {
-				res.status(400).send({
-					code: 400,
-					message: "Invalid Role",
-				})
-			}
-		} catch (error: any) {
-			res.status(500).send({
-				code: 500,
-				message: error.message,
-				stack: error.stack,
-				error: error
-			})
-		}
-	}
-
-	public static is(requiredPermission: string) {
-		const authorizedRole = async (
-			req: Request,
-			res: Response,
-			next: NextFunction
-		) => {
-			//Get JWT From Headers
-			//check null or not if not than replace bearer with blank string
-			const headerAuth = req.headers.authorization;
-			// console.log(headerAuth);
-			if (!headerAuth) {
-				return res.status(400).json({
-					message: 'invalid token',
-				});
-			}
-			const replaceHeaders = headerAuth.replace('Bearer ', '');
-			// console.log(replaceHeaders)
-			//Decode JWT => from USER DATA
-			const decoded: any = jwt.decode(replaceHeaders);
-			// console.log(decoded)
-			const roleId = decoded.found.roles.id;
-			// console.log(roleId);
-			//Find Role from role table -> Invalid Role
-			try {
-				const roleFind = await datasource.getRepository(Roles).findOne({
-					relations: {
-						permission: true,
-					},
-					where: {
-						id: roleId,
-					},
-				});
-				console.log(roleFind);
-				// const found = roleFind.find((role:any) => decodedRole === roleFinder)
-				if (roleFind) {
-					const findPermission = roleFind.permission.some(
-						(permission: any) => permission.name === requiredPermission
-					);
-					console.log(findPermission);
-					if (findPermission) {
-						next;
-					} else {
-						res.status(403).send({
-							code: 403,
-							message: "You don't have enough permission",
-						});
-					}
-				} else {
-					res.status(400).send({
-						code: 400,
-						message: 'Invalid Role',
-					});
-				}
-			} catch (error: any) {
-				res.status(500).send({
-					code: 500,
-					message: error.message,
-					stack: error.stack,
-					error: error,
-				});
-			}
-		};
-
-		return authorizedRole;
-	}
+  public static validateRole = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    requiredPermission: string
+  ) => {
+    //Get JWT From Headers
+    //check null or not if not than replace bearer with blank string
+    const headerAuth = req.headers.authorization;
+    // console.log(headerAuth);
+    if (!headerAuth) {
+      return res.status(400).json({
+        message: 'invalid token',
+      });
+    }
+    const replaceHeaders = headerAuth.replace('Bearer ', '');
+    // console.log(replaceHeaders)
+    //Decode JWT => from USER DATA
+    const decoded: any = jwt.decode(replaceHeaders);
+    // console.log(decoded)
+    const roleId = decoded.found.roles.id;
+    // console.log(roleId);
+    //Find Role from role table -> Invalid Role
+    try {
+      const roleFind = await datasource.getRepository(Roles).findOne({
+        relations: {
+          permission: true,
+        },
+        where: {
+          id: roleId,
+        },
+      });
+      console.log(roleFind);
+      // const found = roleFind.find((role:any) => decodedRole === roleFinder)
+      if (roleFind) {
+        const findPermission = roleFind.permission.some(
+          (permission: any) => permission.name === requiredPermission
+        );
+        console.log(findPermission);
+        if (findPermission) {
+          next;
+        } else {
+          res.status(403).json({
+            code: 403,
+            message: "You don't have enough permission",
+          });
+        }
+      } else {
+        res.status(400).json({
+          code: 400,
+          message: 'Invalid Role',
+        });
+      }
+    } catch (error: any) {
+      res.status(500).json({
+        code: 500,
+        message: error.message,
+        stack: error.stack,
+        error: error,
+      });
+    }
+  };
 }
-
-
